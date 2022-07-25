3af6e225f763462e993fdf7be0c0b6af2b77ef24
Denzhosting <108479508+Denzhosting@users.noreply.github.com>
2022-07-25 22:01:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283897021821','628113048247','6288292024190']
-global.premium = ['6283897021821']
-global.packname = 'Nandi Bot-MD'
-global.author = 'Whatsap Bot'
+global.owner = ['6289525332839','6289676190615','6289525332839']
+global.premium = ['6289525332839']
+global.packname = 'Denz Bot-XD'
+global.author = '© Whatsap Bot|DenzXD'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
